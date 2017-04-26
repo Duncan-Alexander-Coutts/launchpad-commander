720caa905ecd9389ec57d63a40f504cebba94bda
Duncan Coutts <duncan.alex.coutts@googlemail.com>
2017-04-26 20:14:46 +0100

    Migrating Tests to ExpectJs - Re-adding the object util test to the build

--- launchpad-server/src/util/object-util.js
+++ launchpad-server/src/util/object-util.js
@@ -1,5 +1,11 @@
+function hasProperty(targetObject, propertyName) {
+  if (!targetObject || !propertyName) {
+    throw new Error('Both parameters are required');
+  }
+
+  return Object.prototype.hasOwnProperty.call(targetObject, propertyName);
+}
+
 module.exports = {
-  hasProperty(targetObject, propertyName) {
-    return Object.prototype.hasOwnProperty.call(targetObject, propertyName);
-  },
+  hasProperty,
 };
--- launchpad-server/test/util/object-util.spec.js
+++ launchpad-server/test/util/object-util.spec.js
@@ -2,7 +2,61 @@ const expect = require('expect');
 const objectUtil = require('../../src/util/object-util.js');
 
 describe('object-util.spec', () => {
-  it('test', () => {
-    expect(objectUtil.hasProperty).toBeA(Function);
+  describe('hasProperty', () => {
+    describe('when both parameters are undefined', () => {
+      it('should throw an error', () => {
+        expect(() => {
+          objectUtil.hasProperty(undefined, undefined);
+        }).toThrow('Both parameters are required');
+      });
+    });
+
+    describe('when the object parameter is undefined', () => {
+      it('should throw an error', () => {
+        expect(() => {
+          objectUtil.hasProperty(undefined, {});
+        }).toThrow('Both parameters are required');
+      });
+    });
+
+    describe('when the propertyName parameter is undefined', () => {
+      it('should throw an error', () => {
+        expect(() => {
+          objectUtil.hasProperty({}, undefined);
+        }).toThrow('Both parameters are required');
+      });
+    });
+
+    describe('when the propertyName parameter is blank', () => {
+      it('should throw an error', () => {
+        expect(() => {
+          objectUtil.hasProperty({}, '');
+        }).toThrow('Both parameters are required');
+      });
+    });
+
+    describe('when the given object does not have the given property name', () => {
+      it('should return false', () => {
+        expect(objectUtil.hasProperty({}, 'doesNotHaveProperty')).toBe(false);
+      });
+    });
+
+    describe('when the given object has a property but with a different case', () => {
+      it('should return true', () => {
+        const mockObject = {
+          MOCKPROPERTY: '',
+        };
+        expect(objectUtil.hasProperty(mockObject, 'mockProperty')).toBe(false);
+      });
+    });
+
+    describe('when the given object has the given property name', () => {
+      it('should return true', () => {
+        const mockObject = {
+          mockProperty: '',
+        };
+        expect(objectUtil.hasProperty(mockObject, 'mockProperty')).toBe(true);
+      });
+    });
   });
 });
